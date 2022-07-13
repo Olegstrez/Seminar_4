dd1601c2ee6e85ff7c61193239e9880a2c0472ff
Алла Седорина <kornilov.s@evrogas.local>
2022-07-13 15:13:23 +0300

    Changes 27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -9,16 +9,11 @@
 int x = Convert.ToInt32(Console.ReadLine());
 string str = x.ToString();
 int[] b = new int[str.Length];
-for ( int i = 0;i< str.Length; i++)
-{
-b [i] = int.Parse(Convert.ToString(x)[i].ToString());
-}
 int sum = 0;
 for ( int i = 0;i< str.Length; i++)
 {
-    
+    b [i] = int.Parse(Convert.ToString(x)[i].ToString());
     sum = b [i] + sum ;
-
 }
 
 Console.WriteLine($"Сумма чисел = {sum}");
