1613526caefe0ce777fb7c81acd4d376f80322db
Алла Седорина <kornilov.s@evrogas.local>
2022-07-13 15:08:58 +0300

    Добавил задачу 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+Console.WriteLine("Введите массив ширина");
+int ShirinaMasia = Convert.ToInt32(Console.ReadLine());
+int [] Massib  = new int[ShirinaMasia];
+for (int i =0; i < ShirinaMasia; i++)
+{
+    Massib[i] = new Random().Next(1,100);
+    Console.WriteLine($"A {i} = {Massib[i]}");
+}
