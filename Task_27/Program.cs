b028b84ab5451d993a6c399b7fc94714c63a3ae8
Алла Седорина <kornilov.s@evrogas.local>
2022-07-13 14:39:39 +0300

    Добавил задачу 27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -1,4 +1,25 @@
-﻿
-double x = Convert.ToInt16(Console.Read());
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+452 -> 11
 
+82 -> 10
+
+9012 -> 12*/
+
+int x = Convert.ToInt32(Console.ReadLine());
+string str = x.ToString();
+int[] b = new int[str.Length];
+for ( int i = 0;i< str.Length; i++)
+{
+b [i] = int.Parse(Convert.ToString(x)[i].ToString());
+}
+int sum = 0;
+for ( int i = 0;i< str.Length; i++)
+{
+    
+    sum = b [i] + sum ;
+
+}
+
+Console.WriteLine($"Сумма чисел = {sum}");
+ 
